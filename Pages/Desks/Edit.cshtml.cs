b45996358724f48aa6b01b1f676a17940b56f6a7
netozao <andrefsneto@gmail.com>
2020-02-28 22:33:51 -0700

    week 8

---
+++ Pages/Desks/Edit.cshtml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MegaDeskRazorPages.Models;
+using MegaDeskWeb2.Data;
+
+namespace MegaDeskWeb2
+{
+    public class EditModel : PageModel
+    {
+        private readonly MegaDeskWeb2.Data.MegaDeskWeb2Context _context;
+
+        public EditModel(MegaDeskWeb2.Data.MegaDeskWeb2Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public DeskQuote DeskQuote { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            Console.WriteLine("GOT HERE");
+            ViewData["DeliveryId"] = new SelectList(_context.Set<Delivery>(), "DeliveryId", "DeliveryName");
+            ViewData["DesktopMaterialId"] = new SelectList(_context.Set<DesktopMaterial>(), "DesktopMaterialId", "DesktopMaterialName");
+
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            DeskQuote = await _context.DeskQuote.FirstOrDefaultAsync(m => m.DeskQuoteId == id);
+
+            if (DeskQuote == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(DeskQuote).State = EntityState.Modified;
+
+            try
+            {
+
+                DeskQuote.QuotePrice = DeskQuote.GetQuotePrice(_context);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DeskQuoteExists(DeskQuote.DeskQuoteId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool DeskQuoteExists(int id)
+        {
+            return _context.DeskQuote.Any(e => e.DeskQuoteId == id);
+        }
+    }
+}
---
+++ Pages/Desks/Index.cshtml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MegaDeskWeb2.Data;
+using MegaDeskRazorPages.Models;
+
+namespace MegaDeskWeb2
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MegaDeskWeb2Context _context;
+
+        public IndexModel(MegaDeskWeb2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<DeskQuote> DeskQuote { get; set; }
+
+        [BindProperty(SupportsGet =true)]
+        public string searchString { get; set; }
+        
+        
+        [BindProperty(SupportsGet = true)]
+        public string NameSort { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string DateSort { get; set; }
+
+
+
+      
+
+        public async Task OnGetAsync(string sortOrder, string searchString)
+        {
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            var deskQuote = from m in _context.DeskQuote
+                            select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                deskQuote = deskQuote.Where(s => s.CustomerName.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(sortOrder))
+            {
+                switch (sortOrder)
+                {
+                    case "name_desc":
+                        deskQuote = deskQuote.OrderByDescending(m => m.CustomerName);
+                        break;
+                    case "Date":
+                        deskQuote = deskQuote.OrderBy(m => m.Date);
+                        break;
+                    case "date_desc":
+                        deskQuote = deskQuote.OrderByDescending(m => m.Date);
+                        break;
+                    default:
+                        deskQuote = deskQuote.OrderBy(m => m.CustomerName);
+                        break;
+                }
+            }
+
+            DeskQuote = await deskQuote.AsNoTracking().ToListAsync();
+      /*      DeskQuote = await _context.DeskQuote.ToListAsync();*/
+        }
+    }
+}
